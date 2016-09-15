aeb56da88d21be2da4a1487415e07689514fed0d
Manikant Upadhyay <manikant.upadhyay@magicsw.com>
2016-09-15 14:57:31 +0530

    View changes

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,17 +10,6 @@
 angular.module('demoClientApp')
   .controller('LoginCtrl', function ($scope) {
     $scope.login = [
-      {
-      title: 'A New Hope',
-      url: 'http://youtube.com/embed/1g3_CFmnU7k'
-    },
-    {
-      title: 'The Empire Strikes Back',
-      url: 'http://youtube.com/embed/96v4XraJEPI'
-    },
-    {
-      title: 'Return of the Jedi',
-      url: 'http://youtube.com/embed/5UfA_aKBGMc'
-    }
+     
     ];
   });
